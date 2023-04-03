4e94686d56d9b4c6276c71d3f0a4f118f4e93c2c
Rosa <rosa.llorente.alonso@alumnos.upm.es>
2023-04-03 18:43:26 +0200

    commit

---
+++ src/main/java/es/upm/dit/isst/grupo01/medcon01/Medcon01Application.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.grupo01.medcon01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Medcon01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Medcon01Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/upm/dit/isst/grupo01/medcon01/Medcon01ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.grupo01.medcon01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Medcon01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
