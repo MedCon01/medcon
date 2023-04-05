3aa161c63903a5928a5b0828f594b57bc6d60f16
davidpp22 <129781349+davidpp22@users.noreply.github.com>
2023-04-05 21:19:52 +0200

    Add files via upload

--- src/main/java/es/upm/dit/isst/grupo01/medcon01/controller/LoginControllerMedico.java
+++ src/main/java/es/upm/dit/isst/grupo01/medcon01/controller/LoginControllerMedico.java
@@ -1,14 +1,16 @@
-/*
 package es.upm.dit.isst.grupo01.medcon01.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+import java.util.*;
 
 @Controller
-public class LoginControllerMedico {
+public class LoginController {
   
     @GetMapping("/")
     public String showIndexPage() {
@@ -24,25 +26,50 @@ public String showErrorPage() {
     public String showLoginPage() {
         return "login";
     }
+    List<String> items = Arrays.asList("Raúl Cervantes Nuñez", "María Rodriguez Ruíz", "Ana López Bilbao");
+
+    @GetMapping("/welcome")
+    public String showWelcomePage(Model model) {
+        
+        model.addAttribute("items", items);
+        // Agregar el atributo "username" al modelo
+        model.addAttribute("username", "Arturo Pérez Sevilla");
+        return "welcome";
+    }
 
     @PostMapping("/login")
     public String processLoginForm(@RequestParam String username, @RequestParam String password, Model model) {
         // Aquí se realizaría la autenticación del usuario
         // Si el usuario es autenticado exitosamente, se mostraría la página de bienvenida
         // De lo contrario, se volvería a mostrar la página de inicio de sesión con un mensaje de error
-        if (username.equals("usuario") && password.equals("contraseña")) {
+        if (username.equals("12345678A") && password.equals("contraseña")) {
+            model.addAttribute("username", username); // AQUÍ
+            return "redirect:/welcome";
+        } else {
+            model.addAttribute("error", "Nombre de usuario o contraseña incorrectos");
+            return "loginerror";
+        }
+    }
+
+    @PostMapping("/loginerror")
+    public String processLoginErrorForm(@RequestParam String username, @RequestParam String password, Model model) {
+        // Aquí se realizaría la autenticación del usuario
+        // Si el usuario es autenticado exitosamente, se mostraría la página de bienvenida
+        // De lo contrario, se volvería a mostrar la página de inicio de sesión con un mensaje de error
+        if (username.equals("12345678A") && password.equals("contraseña")) {
             model.addAttribute("username", username);
             return "welcome";
         } else {
             model.addAttribute("error", "Nombre de usuario o contraseña incorrectos");
-            return "login";
+            return "loginerror";
         }
     }
 
-    @GetMapping("/welcome")
-    public String showWelcomePage(@RequestParam String username, Model model) {
-        model.addAttribute("username", username);
-        return "welcome";
+    
+    @GetMapping("/paciente")
+    public String mostrarPaciente(Model model) {
+        return "paciente";
     }
+
+
 }
-*/
