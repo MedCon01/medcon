4e4be0819d411d0ab9b96b05a6fa0759030ffe40
Rosa <rosa.llorente.alonso@alumnos.upm.es>
2023-04-07 22:22:13 +0200

    act

--- src/main/java/es/upm/dit/isst/grupo01/medcon01/repository/PacienteRepository.java
+++ src/main/java/es/upm/dit/isst/grupo01/medcon01/repository/PacienteRepository.java
@@ -9,15 +9,9 @@ import es.upm.dit.isst.grupo01.medcon01.model.Paciente;
 
 public interface PacienteRepository extends CrudRepository<Paciente, String> {
     // aquí se añadirán las cosas que pueda devolver el repositorio de paciente
-<<<<<<< HEAD
-    Paciente findByDni(String dni);
-    Paciente findBynTarjeta(Long nTarjeta);
-=======
     Paciente findByDni (String dni);
-    Paciente findByNumTarjeta (int numTarjeta);
     Paciente findTopByOrderByOrdenDesc();
     Paciente findTop1ByCita_HoraAfterOrderByCita_HoraAsc(LocalDateTime horaLlegadaParsed);
->>>>>>> c42f1bf6227b40e1856612068f8b0d0986560f78
     
 }
 
